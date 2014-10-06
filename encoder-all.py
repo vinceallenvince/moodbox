10dab2d50795b31db31b044e85f1d152458cb33a
Vince Allen <vince@spotify.com>
2014-10-06 17:56:09 -0400

    Updates encoder-all.py

--- encoder-all.py
+++ encoder-all.py
@@ -12,6 +12,23 @@ GPIO.setup(input_channel_B, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 old_channel_a = True
 old_channel_b = True
 
+def get_channel_turn():
+    # return -1, 0, or +1
+    global old_channel_a, old_channel_b
+    result = 0
+    new_a = GPIO.input(input_channel_A)
+    new_b = GPIO.input(input_channel_B)
+    if new_a != old_channel_a or new_b != old_channel_b :
+        if old_channel_a == 0 and new_a == 1 :
+            result = (old_channel_b * 2 - 1)
+        elif old_channel_b == 0 and new_b == 1 :
+            result = -(old_channel_a * 2 - 1)
+    old_channel_a, old_channel_b = new_a, new_b
+    time.sleep(0.001)
+    return result
+
+x = 0
+
 ###################
 
 input_volume_A = 18
@@ -39,23 +56,6 @@ def get_volume_turn():
     return result
 
 y = 0
-
-def get_channel_turn():
-    # return -1, 0, or +1
-    global old_channel_a, old_channel_b
-    result = 0
-    new_a = GPIO.input(old_channel_a)
-    new_b = GPIO.input(old_channel_b)
-    if new_a != old_channel_a or new_b != old_channel_b :
-        if old_channel_a == 0 and new_a == 1 :
-            result = (old_channel_b * 2 - 1)
-        elif old_channel_b == 0 and new_b == 1 :
-            result = -(old_channel_a * 2 - 1)
-    old_channel_a, old_channel_b = new_a, new_b
-    time.sleep(0.001)
-    return result
-
-x = 0
 
 while True:
     change_channel = get_channel_turn()
