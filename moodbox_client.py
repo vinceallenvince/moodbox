ef51248023e19afe0ca593b8aeb279a8a0cd38c5
Vince Allen <vince@spotify.com>
2014-10-07 12:30:47 -0400

    Updates moodbox_client.py

--- moodbox_client.py
+++ moodbox_client.py
@@ -102,11 +102,12 @@ def check_ready():
             response = urlopen(req)
         except URLError as e:
             if hasattr(e, 'reason'):
-                LED_count += 1
-                if LED_count % 2 == 0 :
+
+                if LED_count % 2 == 0:
                     led_on()
-                    else:
-                        led_off()
+                else:
+                    led_off()
+
                 #print 'We failed to reach a server.'
                 #print 'Reason: ', e.reason
             elif hasattr(e, 'code'):
