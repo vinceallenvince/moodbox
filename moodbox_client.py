093900f1af1cc944b28d847e6e20cc5d969d3638
vinceallenvince <info@vinceallen.com>
2014-10-12 14:35:02 -0400

    Updates volume range.

--- moodbox_client.py
+++ moodbox_client.py
@@ -106,7 +106,7 @@ def set_channel(x):
 
 def set_volume(y):
     vol = scale(y, (0.0, +10.0), (+50.0, +100.0)) + init_volume_val
-    if vol <= 100:
+    if vol <= 100 and vol >= 0:
         call("amixer set PCM " + `vol` + "%", shell=True)
 
 def set_max_connect_volume():
